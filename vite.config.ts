ab9a9286ec1bd0af657b4256607c31214e8cf778
Ibrahim Tolegen <ibrahim.tolegen06@gmail.com>
2026-01-24 19:16:25 +0500

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,21 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/ecoprocess/',
+export default defineConfig(({ mode }) => ({
+  // mode === 'github' -> for GitHub Pages (https://tolbra.github.io/ecoprocess/)
+  // mode === 'prod' or default -> for custom domain / hosting (https://ecoprocess.kz/)
+  base: mode === 'github' ? '/ecoprocess/' : '/',
+
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+}))
